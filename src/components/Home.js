6ecb209b89df765abe9853f9189c78a4fd899e4b
Michelle Resian <you@example.com>
2024-02-01 23:04:53 +0300

    basics lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home(props) {
   return (
     <div id="home">
       <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
+        Resian is a Web Developer from Kenya
       </h1>
     </div>
   );
